32943467f001ec5c7f471362cc13917c8946fb58
kelsieszost <owner@kelsieszost.design>
2022-12-16 12:32:11 -0500

    add typeDefs

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Query {
+    me: User
+  }
+  input savedBook {
+    authors: [String]
+    description: String
+    title: String
+    bookId: ID!
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
+
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -41,7 +43,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
     });
   });
   db.on("error", (err) => {
-    console.error("error connecting to MongoDB", err);
+    console.error("error connecting", err);
   });
 };
 
